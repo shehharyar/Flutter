eb5738e07ce884e1c5f6430f46b24a5277cdbdac
shehharyar <sheharyarhassan1456@gmail.com>
2023-02-02 12:10:06 +0500

    add models & widgets directories

--- projects/_expense_app/lib/main.dart
+++ projects/_expense_app/lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
+import './widgets/user_transaction.dart';
 import 'package:flutter/material.dart';
-import './transaction.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,13 +19,8 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({super.key});
-  final List<Transaction> transactions = [
-    Transaction(
-        id: "t1", title: "New Shoes", amount: 60.99, date: DateTime.now()),
-    Transaction(
-        id: "t2", title: "Macbook", amount: 90.99, date: DateTime.now()),
-    Transaction(id: "t3", title: "Iphone", amount: 99.99, date: DateTime.now()),
-  ];
+  // late String titleInput;
+  // late String amountInput;
 
   @override
   Widget build(BuildContext context) {
@@ -34,46 +29,21 @@ class MyHomePage extends StatelessWidget {
           title: const Text("Flutter App"),
         ),
         // ignore: prefer_const_constructors
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blueAccent,
-                child: Text("CHART"),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            // mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blueAccent,
+                  child: Text("CHART"),
+                ),
               ),
-            ),
-            Column(
-              children: transactions
-                  .map((tx) => Card(
-                        child: Row(children: [
-                          Container(
-                            margin: EdgeInsets.symmetric(
-                                vertical: 10.0, horizontal: 15.0),
-                            decoration: BoxDecoration(
-                              border:
-                                  Border.all(color: Colors.purple, width: 2),
-                            ),
-                            padding: EdgeInsets.all(10),
-                            child: Text(tx.amount.toString(),
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 17,
-                                    color: Colors.purple)),
-                          ),
-                          Column(
-                            children: [
-                              Text(tx.title),
-                              Text(tx.date.toString()),
-                            ],
-                          )
-                        ]),
-                      ))
-                  .toList(),
-            )
-          ],
+              UserTransactions(),
+            ],
+          ),
         ));
   }
 }
