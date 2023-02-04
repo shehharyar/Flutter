bb9e6815a49f93a3bb71d4279d96c84c33605dc1
shehharyar <sheharyarhassan1456@gmail.com>
2023-02-04 19:40:55 +0500

    added chart amd chart bar files

---
+++ projects/_expense_app/lib/widgets/chart_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double spendingAmount;
+  final double spendingPctOfTotal;
+  const ChartBar(this.label, this.spendingAmount, this.spendingPctOfTotal,
+      {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        SizedBox(
+          height: 4,
+        ),
+        Container(
+          height: 60,
+          width: 6,
+          child: Stack(children: [
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1.0),
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+            FractionallySizedBox(
+              heightFactor: spendingPctOfTotal,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+            )
+          ]),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Text(label)
+      ],
+    );
+  }
+}
--- projects/_expense_app/lib/widgets/transaction_list.dart
+++ projects/_expense_app/lib/widgets/transaction_list.dart
@@ -12,36 +12,56 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-          return Card(
-            child: Row(children: [
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                      color: Theme.of(context).primaryColor, width: 2),
+      child: transactions.isEmpty
+          ? Column(
+              children: [
+                Text(
+                  "No transactions add yet!",
+                  style: Theme.of(context).textTheme.headline6,
                 ),
-                padding: EdgeInsets.all(10),
-                child: Text(
-                    '\$${transactions[index].amount.toStringAsFixed(2)}',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 17,
-                        color: Theme.of(context).primaryColor)),
-              ),
-              Column(
-                children: [
-                  Text(transactions[index].title,
-                      style: Theme.of(context).textTheme.headline6),
-                  Text(DateFormat.yMMMd().format(transactions[index].date)),
-                ],
-              )
-            ]),
-          );
-        },
-        itemCount: transactions.length,
-      ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    height: 200,
+                    child: Image.asset(
+                      "assets/images/waiting.png",
+                      fit: BoxFit.cover,
+                    ))
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Row(children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 15.0),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Theme.of(context).primaryColor, width: 2),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                          '\$${transactions[index].amount.toStringAsFixed(2)}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17,
+                              color: Theme.of(context).primaryColor)),
+                    ),
+                    Column(
+                      children: [
+                        Text(transactions[index].title,
+                            style: Theme.of(context).textTheme.headline6),
+                        Text(DateFormat.yMMMd()
+                            .format(transactions[index].date)),
+                      ],
+                    )
+                  ]),
+                );
+              },
+              itemCount: transactions.length,
+            ),
     );
   }
 }
