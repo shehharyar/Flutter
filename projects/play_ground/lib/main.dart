d276506094ef6e748e9d1012cc77342e90095464
shehharyar <sheharyarhassan1456@gmail.com>
2023-02-05 17:55:39 +0500

    Added play_ground project for test things out

---
+++ projects/play_ground/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return MaterialApp(
+      title: "Playground App",
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Playground App"),
+        ),
+        body: Container(
+          child: Row(),
+        ));
+  }
+}
