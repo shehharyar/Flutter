2d10cf3927b96785f1c4568fd1dc068535cb8a84
shehharyar <sheharyarhassan1456@gmail.com>
2023-04-26 22:53:41 +0500

    added edit product screen

---
+++ projects/_shop_app/lib/screens/edit_product_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class EditProductScreen extends StatefulWidget {
+  const EditProductScreen({super.key});
+  static const routeName = "/edit-product";
+  @override
+  State<EditProductScreen> createState() => _EditProductScreenState();
+}
+
+class _EditProductScreenState extends State<EditProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Edit Product"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+            child: ListView(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(labelText: "Title"),
+              textInputAction: TextInputAction.next,
+            )
+          ],
+        )),
+      ),
+    );
+  }
+}
--- projects/_shop_app/lib/screens/user_products_screen.dart
+++ projects/_shop_app/lib/screens/user_products_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import '../providers/products.dart';
 import '../widgets/user_product_item.dart';
 import "../widgets/app_drawer.dart";
+import './edit_product_screen.dart';
 
 class UserProductsScreen extends StatelessWidget {
   static const routeName = "/user-products";
@@ -14,7 +15,13 @@ class UserProductsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Your Products"),
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(EditProductScreen.routeName);
+              },
+              icon: const Icon(Icons.add))
+        ],
       ),
       // backgroundColor: Color.fromARGB(255, 83, 83, 83),
       drawer: const AppDrawer(),
