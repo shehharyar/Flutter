c98df77e49dbf22be8099b5c5310e92ce30eb408
shehharyar <sheharyarhassan1456@gmail.com>
2023-09-21 11:15:23 +0500

    Added implicit animation

--- projects/meals_app_updated/lib/screens/meal-detail.dart
+++ projects/meals_app_updated/lib/screens/meal-detail.dart
@@ -33,7 +33,15 @@ class MealDetailsScreen extends ConsumerWidget{
                   )
                   );
               // onToggleFavorite(meal);
-              }, icon: Icon(isFavorite? Icons.star : Icons.star_border))
+              }, icon: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                transitionBuilder: (child, animation) {
+                  return RotationTransition(turns: Tween<double>(begin: 0.5, end: 1).animate(animation), child: child,);
+                },      
+                child: Icon(
+                  isFavorite? Icons.star : Icons.star_border,
+                  key: ValueKey(isFavorite),
+                  )))
           ],
         ),
         body: SingleChildScrollView(
