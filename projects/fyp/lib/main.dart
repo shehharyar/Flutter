f754f47d91e43434aa1b1aa663bd2c1057b481b6
shehharyar <sheharyarhassan1456@gmail.com>
2024-02-14 11:11:28 +0500

    added Splash

--- projects/fyp/lib/main.dart
+++ projects/fyp/lib/main.dart
@@ -3,8 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fyp/screens/auth/auth.dart';
-// import 'package:fyp/screens/business/business-reg.dart';
-import 'package:fyp/screens/loading.dart';
 import 'package:fyp/screens/shop/create-shop.dart';
 
 import 'firebase_options.dart';
@@ -31,16 +29,86 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         
       ),
-      home: 
-      StreamBuilder(stream: FirebaseAuth.instance.authStateChanges(), builder: (ctx, snapshot){
-        if(snapshot.connectionState == ConnectionState.waiting){
-          return const LoadingScreen();
-        }
-        if(snapshot.hasData){
-          return const CreateShopScreen();
-        }
-        return const AuthScreen();
-      })
+      home: SplashScreen(),
+   
     );
+  }
+}
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      
+//         useMaterial3: true,
+        
+//       ),
+//       home: 
+//       StreamBuilder(stream: FirebaseAuth.instance.authStateChanges(), builder: (ctx, snapshot){
+//         if(snapshot.connectionState == ConnectionState.waiting){
+//           return const LoadingScreen();
+//         }
+//         if(snapshot.hasData){
+//           return const CreateShopScreen();
+//         }
+//         return const AuthScreen();
+//       })
+//     );
+//   }
+// }
+
+ 
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    initializeAppAndNavigate();
+  }
+
+  Future<void> initializeAppAndNavigate() async {
+    await Future.delayed(const Duration(seconds: 3)); // Simulate a delay for the splash screen
+
+    // Check authentication state and navigate accordingly
+    final User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const CreateShopScreen()),
+      );
+    } else {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const AuthScreen()),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // You can customize your splash screen UI here
+    return  Container(
+              width: 500,
+          height:double.infinity,
+           decoration: const  BoxDecoration(
+          gradient: LinearGradient(  
+            begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+            colors: [Color.fromRGBO(27,9,61,1.000),Color.fromRGBO(82,36,91,1.000),])
+        ),
+        child: Center(
+        child: Image.asset('assets/images/swift.png',
+        fit: BoxFit.cover,
+        )
+        ));
   }
 }
